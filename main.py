19a1899e256c93a3eab13c39add0624eda461142
HarshitaV123 <100452399+HarshitaV123@users.noreply.github.com>
2025-04-27 12:52:38 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -35,4 +35,55 @@ high_score = 0
 class Player(object):
     def __init__(self):
         self.img = rocket
+        self.w = self.img.get_width()
+        self.h = self.img.get_height()
+        self.x = width//2
+        self.y = height//2
+        self.angle = 0
+        self.rotatedSurf = pygame.transform.rotate(self.img, self.angle)
+        self.rotatedRect = self.rotatedSurf.get_rect()
+        self.rotatedRect.center = (self.x, self.y)
+        self.cosine = math.cos(math.radians(self.angle + 90))
+        self.sine = math.sin(math.radians(self.angle + 90))
+        self.head = (self.x + self.cosine*self.w//2, self.y - self.sine*self.h//2)
+    
+    def draw(self,window):
+        window.blit(self.rotatedSurf, self.rotatedRect)
         
+    def turnLeft(self):
+        self.angle += 5
+        self.rotatedSurf = pygame.transform.rotate(self.img,self.angle)
+        self.rotatedRect = self.rotatedSurf.get_rect()
+        self.rotatedRect.center = (self.x, self.y)
+        self.cosine = math.cos(math.radians(self.angle + 90))
+        self.sine = math.sin(math.radians(self.angle + 90))
+        self.head = (self.x + self.cosine* self.w//2, self.y - self.sine*self.h//2)
+
+    def turnRight(self):
+        self.angle -= 5
+        self.rotatedSurf = pygame.transform.rotate(self.img, self.angle)
+        self.rotatedRect = self.rotatedSurf.get_rect()
+        self.rotatedRect.center = (self.x, self.y)
+        self.cosine = math.cos(math.radians(self.angle + 90))
+        self.sine = math.sin(math.radians(self.angle + 90))
+        self.head = (self.x + self.cosine*self.w//2, self.y - self.sine*self.h//2)
+    
+    def moveForward(self):
+        self.x += self.cosine * 6
+        self.y -= self.sine * 6
+        self.rotatedSurf = pygame.transform.rotate(self.img, self.angle)
+        self.rotatedRect = self.rotatedSurf.get_rect()
+        self.rotatedRect.center = (self.x, self.y)
+        self.cosine = math.cos(math.radians(self.angle + 90))
+        self.sine = math.sin(math.radians(self.angle + 90))
+        self.head = (self.x + self.cosine*self.w//2, self.y - self.sine*self.h//2)
+
+    def updateLocation(self):
+        if self.x > width + 50:
+            self.x = 0
+        elif self.x < 0 - self.w:
+            self.x = width
+        elif self.y < -50:
+            self.y = height
+        elif self.y > height + 50:
+            self.y = 0
